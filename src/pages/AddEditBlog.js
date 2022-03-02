50aece2d51b72a1b1a2590ba41555121af2ce049
NGR <ngr112001@gmail.com>
2022-03-02 16:39:20 +0545

    setup all folder & json server

---
+++ src/pages/AddEditBlog.js
@@ -0,0 +1,27 @@
+// import React from 'react'
+// import { MDBValidation, MDBInput, MDBBtn } from 'mdb-react-ui-kit'
+// import axios from 'axios'
+// import { toast } from 'react-toastify'
+
+// const initialState = {
+//     title: "",
+//     description: "",
+//     category: "",
+//     imageUrl: ""
+// }
+
+// const options = ["Travel", "Fashon", "fitness", "sports", "Food", "Tech"];
+
+
+
+
+
+// // z5prfuuq
+// const AddEditBlog = () => {
+//     const [formvalue, setFormValue] = usestate(initialState);
+//     return (
+//         <div>AddEditBlog</div>
+//     )
+// }
+
+// export default AddEditBlog;
